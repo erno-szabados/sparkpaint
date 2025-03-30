5878652de9ecea89dcbc81455595eab7e71351d3
Erno Szabados <erno.szabados@windowslive.com>
2025-03-30 13:37:32 +0200

    Bump version to 0.14

--- src/main/java/com/esgdev/sparkpaint/ui/HelpMenu.java
+++ src/main/java/com/esgdev/sparkpaint/ui/HelpMenu.java
@@ -33,7 +33,7 @@ public class HelpMenu extends JMenu {
         nameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         
         // Add version
-        JLabel versionLabel = new JLabel("Version 0.13");
+        JLabel versionLabel = new JLabel("Version 0.14");
         versionLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         
         // Add description
